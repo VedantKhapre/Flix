1dcfc2f173c63ae1e457782ec2ff943f9641a069
Cookie <143021151+VedantKhapre@users.noreply.github.com>
2025-01-18 18:46:35 +0530

    Sort by seeders

--- Flix.sh
+++ Flix.sh
@@ -4,7 +4,7 @@ query=$(echo "$1" | sed 's/ /+/g')
 query=$(printf '%s' "$*" | tr ' ' '+' )
 echo "Searching for $query"
 
-movie=$(curl -s https://1337x.pics/xsearch?q=$query/ | grep -Eo "torrent\/[0-9]{7}\/[a-zA-Z0-9?-]*" | head -n 1)
+movie=$(curl -s "https://1337x.pics/xsearch?q=$query&sort=seeders" | grep -Eo "torrent\/[0-9]{7}\/[a-zA-Z0-9?-]*" | head -n 1)
 echo "Movie Found: $movie"
 
 magnet=$(curl -s https://1337x.pics/$movie/ | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*")
