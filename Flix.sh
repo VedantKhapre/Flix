5d629c0ec38d4b73756fb37a80fa4e78e5b874e2
0x01 <143021151+VedantKhapre@users.noreply.github.com>
2025-02-02 15:45:41 +0530

    Selection added

--- Flix.sh
+++ Flix.sh
@@ -4,10 +4,18 @@ query=$(echo "$1" | sed 's/ /+/g')
 query=$(printf '%s' "$*" | tr ' ' '+' )
 echo "Searching for $query"
 
-movie=$(curl -s "https://1337x.pics/xsearch?q=$query&sort=seeders" | grep -Eo "torrent\/[0-9]{7}\/[a-zA-Z0-9?-]*" | head -n 1)
-echo "Movie Found: $movie"
+movies=($(curl -s "https://1337x.pics/xsearch?q=$query&sort=seeders" | grep -Eo "torrent\/[0-9]{7}\/[a-zA-Z0-9?-]*" | head -n 5))
+
+echo "Movies found:"
+for i in "${!movies[@]}"; do
+    echo "$((i+1)). ${movies[$i]##*/}"
+done
+
+read -p "Select a movie (1-${#movies[@]}): " selection
+selection=$((selection-1))
+movie=${movies[$selection]}
 
 magnet=$(curl -s https://1337x.pics/$movie/ | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*")
 echo "Links Fetched: $magnet"
 
-peerflix -k $magnet 
+peerflix -k $magnet --no-peer
